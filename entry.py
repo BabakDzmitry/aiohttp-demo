462967ed3f866010ea269f7dc628c0519828961d
Dzmitry Babak <alexgott1990@gmail.com>
2020-07-15 19:54:08 +0300

    add command line params

--- entry.py
+++ entry.py
@@ -1,14 +1,25 @@
+import argparse
 import aiohttp
 import asyncio
 from demo import create_app
 
 try:
     import uvloop
+
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 except ImportError:
     print('Library uvloop is not available')
 
+parser = argparse.ArgumentParser(description="Demo project")
+parser.add_argument('--host', help='Host to listen', default='127.0.0.1')
+parser.add_argument('--port', help='Port to accept connections',default='8080')
+parser.add_argument('--reload', action='store_true', help='Auto reloaded')
+
+args = parser.parse_args()
+
+if args.reload:
+    print('Start with code reload')
 
 app = create_app()
 if __name__ == '__main__':
-    aiohttp.web.run_app(app)
+    aiohttp.web.run_app(app, host=args.host, port=args.port)
