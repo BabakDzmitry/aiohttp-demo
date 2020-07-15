3eb89b8dd5b1958cd6ce6c7f0c3200ed8bf08864
Dzmitry Babak <alexgott1990@gmail.com>
2020-07-15 19:29:49 +0300

    add: uvloop

--- entry.py
+++ entry.py
@@ -1,6 +1,13 @@
 import aiohttp
-
+import asyncio
 from demo import create_app
+
+try:
+    import uvloop
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+except ImportError:
+    print('Library uvloop is not available')
+
 
 app = create_app()
 if __name__ == '__main__':
