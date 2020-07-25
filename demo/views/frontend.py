ab587ee899e0992b03b6fe99e14add802ef08b17
Dzmitry Babak <alexgott1990@gmail.com>
2020-07-26 02:19:19 +0300

    add config

--- demo/views/frontend.py
+++ demo/views/frontend.py
@@ -1,8 +1,8 @@
 import aiohttp
-
 from aiohttp_jinja2 import template
 
 
 @template('index.html')
 async def index(request):
-    return {}
+    site_name = request.app['config'].get('site_name')
+    return {'site_name': site_name}
--- entry.py
+++ entry.py
@@ -2,10 +2,10 @@ import argparse
 import aiohttp
 import asyncio
 from demo import create_app
+from demo.settings import load_config
 
 try:
     import uvloop
-
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 except ImportError:
     print('Library uvloop is not available')
@@ -15,13 +15,16 @@ parser.add_argument('--host', help='Host to listen', default='127.0.0.1')
 parser.add_argument('--port', help='Port to accept connections', default='8080')
 parser.add_argument('--reload', action='store_true', help='Auto reloaded')
 
+parser.add_argument('-c', '--config', type=argparse.FileType('r'), help='Path to file config')
 args = parser.parse_args()
+
+app = create_app(config=load_config(args.config))
 
 if args.reload:
     print('Start with code reload')
     import aioreloader
     aioreloader.start()
 
-app = create_app()
+
 if __name__ == '__main__':
     aiohttp.web.run_app(app, host=args.host, port=args.port)
