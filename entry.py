d3e022d3a2667608c0de4d19e5f23e9404ee7736
Dzmitry Babak <alexgott1990@gmail.com>
2020-07-25 18:14:15 +0300

    add: reload app

--- entry.py
+++ entry.py
@@ -12,13 +12,15 @@ except ImportError:
 
 parser = argparse.ArgumentParser(description="Demo project")
 parser.add_argument('--host', help='Host to listen', default='127.0.0.1')
-parser.add_argument('--port', help='Port to accept connections',default='8080')
+parser.add_argument('--port', help='Port to accept connections', default='8080')
 parser.add_argument('--reload', action='store_true', help='Auto reloaded')
 
 args = parser.parse_args()
 
 if args.reload:
     print('Start with code reload')
+    import aioreloader
+    aioreloader.start()
 
 app = create_app()
 if __name__ == '__main__':
